65dd0912f1a44cb51ad1889441e058b1b76cad82
uriel <urielsalimora@gmail.com>
2023-09-17 20:12:22 -0600

    Ejercicio 2

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -22,5 +22,38 @@
             }
         }
     ?>
+     <h2>Ejercicio 2</h2>
+    <p>Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
+    secuencia compuesta por:</p>
+    <p>impar, par, impar</p>
+    <p>Estos números deben almacenarse en una matriz de Mx3, donde M es el número de filas y
+    3 el número de columnas. Al final muestra el número de iteraciones y la cantidad de
+    números generados:</p>
+    <?php
+        $ban=true;
+        $total_num=0;
+        $iteraciones=0;
+        do{
+            $numero1 = rand(1, 999);
+            $numero2 = rand(1, 999);
+            $numero3 = rand(1, 999);
+            $iteraciones ++;
+            $total_num += 3;
+
+            $numeros_alt[] = [$numero1, $numero2, $numero3] ;
+
+            if ($numero1 % 2 != 0 && $numero2 % 2 == 0 && $numero3 % 2 != 0) {
+                $ban=false; 
+            }
+        }
+        while ($ban); 
+        //foreach recorrer el array, la cual son los elementos de fila que contiene los elementos de numeros_alt[]
+        foreach ($numeros_alt as $fila) {
+            //implode concatena los elementos en fila en este caso los array y son separados con br
+            echo implode("  ", $fila) . "<br>";
+        }
+        echo "Número de iteraciones: $iteraciones<br>";
+        echo "Total de números generados: $total_num<br>"
+    ?>
 </body>
 </head>
