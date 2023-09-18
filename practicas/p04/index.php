6dae5c4caba20a132e176ed7cf31599c3bac6339
uriel <urielsalimora@gmail.com>
2023-09-17 20:11:14 -0600

    Ejercicio 1

---
+++ practicas/p04/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN”
+“http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang= "es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title> Practica 4: Manejo de variables con PHP </title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    <?php
+        if(isset($_GET['numero']))
+        {
+            $num = $_GET['numero'];
+            if ($num%5==0 && $num%7==0)
+            {
+                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
+            }
+            else
+            {
+                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
+            }
+        }
+    ?>
+</body>
+</head>
